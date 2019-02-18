28a817bdfd74cdd6766705ace7610c2a02cfb87f
nirkaufman <nirkaufman@gmail.com>
2019-02-18 10:13:55 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from '@emotion/styled';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container>
+        <h1>Mastering Hooks</h1>
+      </Container>
     );
   }
 }
+
+const Container = styled.div`
+  padding: 50px;
+`;
 
 export default App;
