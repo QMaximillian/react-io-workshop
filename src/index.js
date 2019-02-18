1f6089906d28288433fe5b4cc1be560ef4fc4307
nirkaufman <nirkaufman@gmail.com>
2019-02-18 10:24:52 -0500

    high level project structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import 'milligram/dist/milligram.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
