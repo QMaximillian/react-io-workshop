ac9e38c5812a2977bbeaf6003330583247b8a914
nirkaufman <nirkaufman@gmail.com>
2019-02-18 20:31:47 -0500

    fix header

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import styled from '@emotion/styled';
 import {StyledLink} from "./StyledLink";
 import {useAuth} from "../../hooks/use-auth";
+import {AuthContext} from "./Auth";
 
 const Header = () => {
   const {logOut} = useAuth();
+  const {user} = useContext(AuthContext);
 
   return (
       <HeaderContainer>
-        <div>Places to eat</div>
+        <div>Places to eat for <UserName>{user.name}</UserName></div>
         <StyledLink to={'/'} onClick={logOut}>Logout</StyledLink>
       </HeaderContainer>
   );
@@ -21,5 +23,9 @@ const HeaderContainer = styled.div`
   margin-bottom: 15px;
   padding: 15px 25px;
 `;
+
+const UserName = styled.span`
+  color: mediumpurple;
+`
 
 export default Header;
